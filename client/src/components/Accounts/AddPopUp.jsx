3ffef6c5b500502fc29911b289398d617596adf2
mukhtar <mukhtarkussaiynbekov@gmail.com>
2021-03-13 21:28:37 +0900

    update styling of pop up add window

--- client/src/components/Accounts/AddPopUp.jsx
+++ client/src/components/Accounts/AddPopUp.jsx
@@ -12,20 +12,39 @@ const AddPopUp = ({ toggle }) => {
 			<div className="popup-message">
 				<div className="popup-content">
 					<div className="popup-block popup-block-income">
-						<div className="create-input create-input-sep">
-							<div className="form-item">
-								<input
-									className="popup-input"
-									placeholder="Where does money come from? *"
-								/>
-								<div className="popup-input-sub popup-input-sub-error">
-									* Enter income source title
+						<div>
+							<div className="create-input create-input-sep">
+								<div className="form-item">
+									<input
+										className="popup-input"
+										placeholder="Where does money come from? *"
+									/>
+									<div className="popup-input-sub popup-input-sub-error">
+										* Enter income source title
+									</div>
+								</div>
+								<div className="icon-picker-icon">
+									<div className="card-item-icon card-item-icon-income">
+										<div className="card-item-icon-miscellaneous"></div>
+									</div>
 								</div>
 							</div>
-							<div className="icon-picker-icon">
-								<div className="card-item-icon card-item-icon-income">
-									<div className="card-item-icon-miscellaneous"></div>
+
+							<div className="create-input create-input-sep">
+								<div className="form-item">
+									<input
+										className="popup-input popup-input-number"
+										placeholder="Planning to receive per month"
+									/>
 								</div>
+							</div>
+
+							<div class="form-action">
+								<input
+									type="button"
+									value="add"
+									class="btn-create btn-create-income"
+								/>
 							</div>
 						</div>
 					</div>
