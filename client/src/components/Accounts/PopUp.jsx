b4f91000cf27072128ef3c8c60ba1cafabfa9261
arnihere <arnur.makenov@gmail.com>
2021-06-01 02:02:47 +0900

    altered config axios request urls

--- client/src/components/Accounts/PopUp.jsx
+++ client/src/components/Accounts/PopUp.jsx
@@ -107,7 +107,7 @@ const PopUp = ({
 			// Make call to the server to change existing data
 			var config = {
 				method: 'put',
-				url: `${API_URL}/${source}`,
+				url: API_URL + source,
 				headers: {
 					...authHeader(),
 					'Content-Type': 'application/json'
@@ -138,7 +138,7 @@ const PopUp = ({
 
 		var config = {
 			method: 'delete',
-			url: `${API_URL}/${source}`,
+			url: API_URL + source,,
 			headers: {
 				...authHeader(),
 				'Content-Type': 'application/json'
