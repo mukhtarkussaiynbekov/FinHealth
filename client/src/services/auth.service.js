459720043aa1ed4e9b7abb4f89c21e9850c5efe5
arnihere <arnur.makenov@gmail.com>
2021-06-01 01:49:35 +0900

    updated API URL

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'http://localhost:8080/';
+export const API_URL = 'https://finhealth2.herokuapp.com';
 
 class AuthService {
 	login(email, password) {
